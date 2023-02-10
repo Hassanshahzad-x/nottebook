7d01a46e8058c476127ca54ecdff321e60de6a40
Hassanshahzad-x <hassanshahzad3513@gmail.com>
2023-02-11 01:51:12 +0500

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      hey
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
